ff86d4e1140d900e64d988faec0c0e6f2ef3dda8
Lilian Wanjiku <113428564+lilintech@users.noreply.github.com>
2024-03-26 22:25:04 +0300

    changed link details

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,9 +5,9 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Lilian Wanjiku",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "geekScripts",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "A blog site by lilintech where learning is a fun and practical experience.",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
